15e3ea433cc1d903ebd0867a732c5290bc8f2b37
Alex Williams <alex.c.williams91@gmail.com>
2019-07-28 09:05:03 +0100

    fix lint issues

--- src/Domain/DataTransferObjects/MovieDto.ts
+++ src/Domain/DataTransferObjects/MovieDto.ts
@@ -1,12 +1,12 @@
 type Certificate = 'U' | 'PG' | '12/12A' | '15' | '18' | 'R18';
 
 interface GenreDTO {
-  genre_id: string;
+  genreId: string;
   name: string;
 }
 
 export interface MovieDTO {
-  movie_id: string;
+  movieId: string;
   title: string;
   duration: number;
   language: string;
@@ -14,4 +14,4 @@ export interface MovieDTO {
   releaseYear: string;
   certificate: Certificate[];
   genres: GenreDTO[];
-};
+}
