e801974ba04601ee3b76a786ca441ecfdece7933
Alex Williams <alex.c.williams91@gmail.com>
2019-07-28 22:08:04 +0100

    prefer route with api

--- tests/Http/Controllers/MovieController.test.js
+++ tests/Http/Controllers/MovieController.test.js
@@ -3,7 +3,7 @@ const app = require('../../../src/app');
 
 describe('MovieController test suite', () => {
   test('will return 200 success on post request', async () => {
-    const test = await request(app).post('/movies');
+    const test = await request(app).post('/api/movies');
     expect(test.status).toBe(200);
   });
 });
