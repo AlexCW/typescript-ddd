7dd048100f53e6178cfde0efc3cf994614e8797c
Alex Williams <alex.c.williams91@gmail.com>
2019-07-29 22:32:44 +0100

    fix tests to use imports after installing babel

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.tsx?$": "ts-jest"
+  }
 };
--- tests/Http/Controllers/MovieController.test.js
+++ tests/Http/Controllers/MovieController.test.js
@@ -1,9 +1,10 @@
-const request = require('supertest');
-const app = require('../../../src/app');
+import request from 'supertest';
+import app from '../../../src/app';
 
 describe('MovieController test suite', () => {
   test('will return 200 success on post request', async () => {
     const test = await request(app).post('/api/movies');
     expect(test.status).toBe(200);
+    console.log(test.links);
   });
 });
